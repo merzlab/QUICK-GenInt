b0df31331751301e89e308ed608c7b4559e3fc7e
Madushanka Manathunga <mmmskumara@gmail.com>
2021-07-01 17:40:05 -0400

    initial commit

---
+++ one_electron_integral.py
@@ -0,0 +1,20 @@
+#!---------------------------------------------------------------------!
+#! Written by Madu Manathunga on 07/01/2021                            !
+#!                                                                     !
+#! Copyright (C) 2020-2021 Merz lab                                    !
+#! Copyright (C) 2020-2021 Götz lab                                    !
+#!                                                                     !
+#! This Source Code Form is subject to the terms of the Mozilla Public !
+#! License, v. 2.0. If a copy of the MPL was not distributed with this !
+#! file, You can obtain one at http://mozilla.org/MPL/2.0/.            !
+#!_____________________________________________________________________!
+
+#!---------------------------------------------------------------------!
+#! This source file contains classes necessary for generating one      !
+#! electron integrals.                                                 !
+#!---------------------------------------------------------------------!
+
+import params
+
+class OEint:
+    pass
